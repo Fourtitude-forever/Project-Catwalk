fbbf936b5fea85cb940a806f559b972ddbc12d6d
Joseph Lio <josephlio@hotmail.com>
2021-04-16 12:30:09 -0700

    Add shared state to App component and set up file structure for modules

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import ProductDetail from './ProductDetail.jsx';
+import RelatedProducts from './RelatedProducts.jsx';
+import QuestionsAndAnswers from './QuestionsAndAnswers.jsx';
+import RatingsAndReviews from './RatingsAndReviews.jsx';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productID: 23145
+    };
+  }
+
   render() {
     return (
-      <div>Hello From App</div>
-    )
+      <div>
+        <div>Hello From App</div>
+        <ProductDetail />
+        <RelatedProducts />
+        <QuestionsAndAnswers />
+        <RatingsAndReviews />
+      </div>
+    );
   }
 }
-
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/',express.static(path.join(__dirname,'../client/dist')));
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
 // app.get('/api/blogs', controller.get);
 
