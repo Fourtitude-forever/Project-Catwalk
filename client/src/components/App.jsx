862c33fbc3799de52db0b37205984f84f8a3e394
Jackson Pham <jackson.pham@gmail.com>
2021-04-19 15:54:37 -0700

    Random Changes to Move Forward

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ce2acdf3e3c72115d4823815081231679be2b9d6
 // import ProductDetail from './ProductDetail.jsx';
 // import RelatedProducts from './RelatedProducts.jsx';
 // import QuestionsAndAnswers from './QuestionsAndAnswers.jsx';
 import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews.jsx';
+<<<<<<< HEAD
 =======
 import QuestionList from './questions/QuestionList.jsx';
 >>>>>>> main
+=======
+import QuestionList from './questions/QuestionList.jsx';
+>>>>>>> ce2acdf3e3c72115d4823815081231679be2b9d6
 
 class App extends React.Component {
   constructor(props) {
@@ -22,13 +29,20 @@ class App extends React.Component {
       <div>
         <div>Hello From App</div>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ce2acdf3e3c72115d4823815081231679be2b9d6
         {/* <ProductDetail productID={productID} />
         <RelatedProducts productID={productID} />
         <QuestionsAndAnswers productID={productID} /> */}
         <RatingsAndReviews productID={productID} />
+<<<<<<< HEAD
 =======
         <QuestionList productID={this.state.productID} />
 >>>>>>> main
+=======
+        <QuestionList productID={this.state.productID} />
+>>>>>>> ce2acdf3e3c72115d4823815081231679be2b9d6
       </div>
     );
   }
--- client/src/components/RelatedProducts.jsx
+++ client/src/components/RelatedProducts.jsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from 'react';
 
 
@@ -6,3 +7,6 @@ export default RelatedProducts;
 =======
 // export default RelatedProducts;
 >>>>>>> main
+=======
+// export default RelatedProducts;
+>>>>>>> ce2acdf3e3c72115d4823815081231679be2b9d6
