892d627013edf7a2cc944f4e349c5a40d4bb27af
Jackson Pham <jackson.pham@gmail.com>
2021-04-19 17:25:09 -0700

    Added a Style Sheet for Star Rating

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ce2acdf3e3c72115d4823815081231679be2b9d6
-=======
->>>>>>> 6bfd6a5fbf0d127b1cf36cec099f831fd4191bea
+
 // import ProductDetail from './ProductDetail.jsx';
 // import RelatedProducts from './RelatedProducts.jsx';
 // import QuestionsAndAnswers from './QuestionsAndAnswers.jsx';
 import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews.jsx';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import QuestionList from './questions/QuestionList.jsx';
->>>>>>> main
-=======
-import QuestionList from './questions/QuestionList.jsx';
->>>>>>> ce2acdf3e3c72115d4823815081231679be2b9d6
-=======
-import QuestionList from './questions/QuestionList.jsx';
->>>>>>> 6bfd6a5fbf0d127b1cf36cec099f831fd4191bea
+
 
 class App extends React.Component {
   constructor(props) {
@@ -35,28 +20,12 @@ class App extends React.Component {
     return (
       <div>
         <div>Hello From App</div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ce2acdf3e3c72115d4823815081231679be2b9d6
-=======
->>>>>>> 6bfd6a5fbf0d127b1cf36cec099f831fd4191bea
+
         {/* <ProductDetail productID={productID} />
         <RelatedProducts productID={productID} />
         <QuestionsAndAnswers productID={productID} /> */}
-        <RatingsAndReviews productID={productID} />
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+        <RatingsAndReviews productID={this.state.productID} />
         <QuestionList productID={this.state.productID} />
->>>>>>> main
-=======
-        <QuestionList productID={this.state.productID} />
->>>>>>> ce2acdf3e3c72115d4823815081231679be2b9d6
-=======
-        <QuestionList productID={this.state.productID} />
->>>>>>> 6bfd6a5fbf0d127b1cf36cec099f831fd4191bea
       </div>
     );
   }
--- client/src/components/RatingsAndReviews/StarRating.jsx
+++ client/src/components/RatingsAndReviews/StarRating.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { FaStar } from 'react-icons/fa';
+import styled from 'styled-components';
 
 // import './StarRating.css';
 
