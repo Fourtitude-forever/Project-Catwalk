18bd189c6ce99ab4e685a733b5755deaf92e50b9
Jackson Pham <jackson.pham@gmail.com>
2021-04-20 09:12:56 -0700

    Moved Styled Components to Top and Removed Unecessary ClassName

--- client/src/components/RatingsAndReviews/Reviews.jsx
+++ client/src/components/RatingsAndReviews/Reviews.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledP = styled.p`
+border-bottom: 2px solid black;
+font-family: 'trebuchet ms'
+`;
+
+const StyledH = styled.h3`
+font-family: 'trebuchet ms'
+`;
+
 const Reviews = ({ reviews, loading }) => {
   if (loading) {
     return <h2>Loading...</h2>;
@@ -8,7 +17,7 @@ const Reviews = ({ reviews, loading }) => {
   // console.log(reviews);
   return (
     <div>
-      <div className="reviews">
+      <div>
         {reviews.map((review) => (
           <div key={review.id}>
             <span><StyledH>{review.title}</StyledH></span>
@@ -19,14 +28,5 @@ const Reviews = ({ reviews, loading }) => {
     </div>
   );
 };
-
-const StyledP = styled.p`
-border-bottom: 2px solid black;
-font-family: 'trebuchet ms'
-`;
-
-const StyledH = styled.h3`
-font-family: 'trebuchet ms'
-`;
 
 export default Reviews;
