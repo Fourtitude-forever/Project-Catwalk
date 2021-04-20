846c12061743f94a578bc2582b7796038b896b61
Benjamin Brandon <benb89.bb@gmail.com>
2021-04-19 23:21:17 -0700

    Axios auth header/get req bug fixed, geting 200 status for hhtps reqs

--- client/src/components/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/RelatedProducts/RelatedProducts.jsx
@@ -1,19 +1,33 @@
-import React, { useState } from 'react';
-import API_KEY from '../../../../config.js';
+import React, { useState, useEffect } from 'react';
+import config from '../../../../config.js';
+import axios from 'axios';
 
-function RelatedProducts() {
+const RelatedProducts = ({ productID }) => {
 
-  const [products, setProducts] = useState([]);
+  const [products, setRelatedProducts] = useState([]);
 
-  //useEffect()
+  useEffect(() => {
+    axios.defaults.headers.common['Authorization'] = config.Autherization;
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/products/', {headers: config})
+      .then((relatedProducts) => {
+        console.log('reaching this');
+        console.log(relatedProducts);
+      })
+      .catch((err) => {
+        console.log(err.response, 'error in the get req')
+      })
+
+    }, [products])
+
 
   return (
+
     <div className="RelatedProducts">
       <h1>Related Products</h1>
       <div className="carousel">
         <div className="productCard">
           <div className="item_1">
-              sampleItem
+              Test Card
           </div>
         </div>
         <div className="carousel_actions">
