8884aabbd1eec97f6ac014abbdbe000b708a2439
Benjamin Brandon <benb89.bb@gmail.com>
2021-04-21 16:53:23 -0700

    removed console.logs and fixed authentication error due to spelling error

--- client/src/components/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/RelatedProducts/RelatedProducts.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import config from '../../../../config.js';
 import axios from 'axios';
+import ProductCard from './ProductCard.jsx'
 
 const RelatedProducts = ({ productID }) => {
 
@@ -8,21 +9,11 @@ const RelatedProducts = ({ productID }) => {
   const [productData, setProductData] = useState([]);
 
 
-
-
   useEffect(() => {
-    axios.defaults.headers.common['Authorization'] = config.Autherization;
     axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/products/${productID}/related`, {headers: config})
       .then((products) => {
-        // console.log('reaching this');
-        // console.log(products)
-        // console.log(products.data);
         setRelatedProducts(products.data);
-        //console.log(relatedProducts, 'after state set')
       })
-      // .then(()=>{
-      //   console.log(relatedProducts);
-      // })
       .catch((err) => {
         console.log(err.response, 'err in req1')
       })
@@ -31,13 +22,7 @@ const RelatedProducts = ({ productID }) => {
     const getHelper = (relatedProduct) => {
       axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/products/${relatedProduct}`, {headers: config})
         .then((product) => {
-          //console.log(product.data);
-          // const data = [product.data];
-          // console.log(productData, 'this is the state')
-          // const previousData = productData;
-          // setProductData({...previousData, data})
           productData.push(product.data)
-          //console.log(productData);
         })
         .catch(() =>{
           console.log(response.err, 'err at req2')
@@ -57,9 +42,7 @@ const RelatedProducts = ({ productID }) => {
       <h1>Related Products</h1>
       <div className="carousel">
         <div className="productCard">
-          <div className="item_1">
-              Test Card
-          </div>
+          <ProductCard />
         </div>
         <div className="carousel_actions">
           <button id="carousel_button--prev" aria-label="Previous_cards">˂</button>
