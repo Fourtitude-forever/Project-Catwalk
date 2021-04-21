a6bdda34da27c3cfd57b127bb25d22e3a011adb5
Jackson Pham <jackson.pham@gmail.com>
2021-04-20 21:08:04 -0700

    Got the Get Request to API to Work

--- client/src/components/RatingsAndReviews/ReviewsList.jsx
+++ client/src/components/RatingsAndReviews/ReviewsList.jsx
@@ -2,23 +2,36 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import Reviews from './Reviews.jsx';
-import config from '../../../../config.js'
+import config from '../../../../config.js';
 
-const ReviewsList = () => {
+const ReviewsList = ({ productID }) => {
   const [reviews, setReviews] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [reviewsPerPage] = useState(2);
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      setLoading(true);
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
-      setReviews(res.data);
-      setLoading(false);
-    };
+    // const fetchPosts = async () => {
+    //   setLoading(true);
+    //   const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    //   setReviews(res.data);
+    //   console.log(res.data);
+    //   setLoading(false);
+    // };
+    // fetchPosts();
+    setLoading(true);
+    axios('https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/reviews/', {
+      headers: config,
+      params: { product_id: productID },
+    })
+      .then((list) => {
+        setReviews(list.data.results);
 
-    fetchPosts();
+        setLoading(false);
+      })
+      .catch((err) => {
+        throw err;
+      });
   }, []);
 
   // Get current reviews
@@ -27,7 +40,7 @@ const ReviewsList = () => {
   const currentReviews = reviews.slice(indexOfFirstReview, indexOfLastReview);
 
   return (
-    <div className="container">
+    <div>
       <h1>Ratings and Reviews</h1>
       <Reviews reviews={currentReviews} loading={loading} />
       <button type="button">More Reviews</button>
