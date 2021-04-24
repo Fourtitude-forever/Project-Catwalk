0af396591f9c32fa2778215e82337dfe13a70c0b
Joseph Lio <josephlio@hotmail.com>
2021-04-23 18:57:37 -0700

    Post clicked info to API for QA component only

--- client/src/components/Interactions/interactions.jsx
+++ client/src/components/Interactions/interactions.jsx
@@ -1,52 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import QuestionList from '../questions/QuestionList.jsx';
+import request from '../../lib/getInfo.js';
+
 
 function withTracking(WrappedComponent, eventHandler) {
   return (props) => {
     const [countClick, setCountClick] = useState(0);
 
     const onComponentClick = (event) => {
-      console.log('click event is: ', event);
-      setCountClick(countClick + 1);
+      event.persist();
+      console.log('clicked widget is: ', WrappedComponent.name);
+      console.log('clicked element is: ', event.target);
+      const timeStamp = new Date().toISOString();
+      console.log('clicked time is: ', timeStamp);
+      request.postInteractionRequest(event.target.toString(), WrappedComponent.name, timeStamp)
+        .then(() => console.log('success'))
+        .catch((err) => console.log(err));
     };
 
-    useEffect(() => {
-      console.log('countClick is ', countClick);
-    }, []);
 
     return (
       <WrappedComponent
-        // data={countClick}
-        onCompClick={(event) => eventHandler.touchStart(event)}
+        onCompClick={(event) => onComponentClick(event)}
         {...props}
       />
     );
   };
 }
-//   return class extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-//         clickCount: 0,
-//       };
-//     }
 
-//     onComponentClick(event) {
-//       console.log('click event is: ', event);
-//       this.setState({ clickCount: this.state.clickCount + 1 });
-//     }
-
-//     render() {
-//       return (
-//         <WrappedComponent
-//           onClick={(e) => this.onComponentClick(e)}
-//           {...this.props}
-//         />
-//       );
-//     }
-//   };
-// }
-
-const QuestionListWithTracking = withTracking(QuestionList, { touchStart: () => console.log('touchStart') });
+const QuestionListWithTracking = withTracking(QuestionList);
 
 export default QuestionListWithTracking;
